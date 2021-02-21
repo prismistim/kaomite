70aa8868e5e89e05614c5494a503ff090bcc35e7
prismistim <prismistim@hotmail.com>
2021-02-22 03:03:00 +0900

    add: マイクの追加とfaceCircleのグローバル変数化

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 let pg
 let capture
 let tracker
+let mic
 
 const windowSize = {
   width: 640,
   height: 480
+}
+
+const faceCircle = {
+  x: 0,
+  y: 0
 }
 
 function setup() {
@@ -27,21 +33,20 @@ function setup() {
   tracker = new clm.tracker()
   tracker.init()
   tracker.start(capture.elt)
+
+  mic = new p5.AudioIn()
+  mic.start()
 }
 
 function draw() {
   const positions = tracker.getCurrentPosition()
+  const micLevel = mic.getLevel()
 
-  pg.background(255,255, 255)
+  pg.background(255, 255, 255)
 
   pg.erase()
   pg.ellipse(pg.width / 2, pg.height / 2, windowSize.height * 0.8, windowSize.height * 0.8)
   pg.noErase()
-
-  const faceCircle = {
-    x: 0,
-    y: 0
-  }
 
   if (positions.length > 0) {
     const facePosition = {
@@ -60,8 +65,15 @@ function draw() {
     } else if (facePosition.y > windowSize.height) {
       faceCircle.y = windowSize.height / 2 + facePosition.y
     }
+  }
 
-    image(capture, faceCircle.x, faceCircle.y, windowSize.width, windowSize.height)
+  image(capture, faceCircle.x, faceCircle.y, windowSize.width, windowSize.height)
+
+  if (micLevel > 0) {
+    noFill()
+    stroke(255, 204, 0)
+    strokeWeight(10)
+    ellipse(windowSize.width / 2, windowSize.height / 2, windowSize.height * 0.7, windowSize.height * 0.7)
   }
 
   image(pg, 0, 0)
